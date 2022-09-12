6f281d4ba4147ed5db07a1319eb589593c181e40
krobinson99 <kmr18@sfu.ca>
2022-09-12 14:47:55 -0700

    function to find closest model gridcell to each snowpit

--- FieldWork2022/snow2022.py
+++ FieldWork2022/snow2022.py
@@ -29,6 +29,7 @@ swe = arr[:,8] #units = cm
 obs_snow = swe/100
 easting_obs = arr[:,2]
 northing_obs = arr[:,3]
+pitnames = arr[:,1]
 
 
 
@@ -96,6 +97,7 @@ def seasonalaccumulation(year1,year2,BC):
     return totalsnow
 
 snow2022_bc =  seasonalaccumulation('F:/Mass Balance Model/BiasCorrectedNARR_1979-2022/Constant_Z/Prcpkaskonly_BC_2021.nc','F:/Mass Balance Model/BiasCorrectedNARR_1979-2022/Constant_Z/Prcpkaskonly_BC_2022.nc',True)
+snow2022_nonbc =  seasonalaccumulation('F:/Mass Balance Model/DownscaledNARR_1979-2022/Constant_Z/netSnowkaskonly2021.nc','F:/Mass Balance Model/DownscaledNARR_1979-2022/Constant_Z/netSnowkaskonly2022.nc',False)
 
 #snow20112012_noBC = seasonalaccumulation('F:/Mass Balance Model/DownscaledNARR/netSnowkaskonly2011.nc','F:/Mass Balance Model/DownscaledNARR/netSnowkaskonly2012.nc',False)
 #snow20112012_BC = seasonalaccumulation('F:/Mass Balance Model/BiasCorrectedInputs/Kaskonly_R2S=1/Prcpkaskonly_BC_2011.nc','F:/Mass Balance Model/BiasCorrectedInputs/Kaskonly_R2S=1/Prcpkaskonly_BC_2011.nc',True)
@@ -111,7 +113,20 @@ plt.ylabel('Northing',fontsize=14)
 plt.xticks(ticks=[0,50,100,150,200,250,300],labels=[int(Xgrid[0][0]),int(Xgrid[0][50]),int(Xgrid[0][100]),int(Xgrid[0][150]),int(Xgrid[0][200]),int(Xgrid[0][250]),int(Xgrid[0][300])],fontsize=10,rotation=20)
 plt.yticks(ticks=[0,50,100,150,200],labels=[int(Ygrid[:,0][0]),int(Ygrid[:,0][50]),int(Ygrid[:,0][100]),int(Ygrid[:,0][150]),int(Ygrid[:,0][200])],fontsize=10,rotation=20)
 plt.title('Bias corrected NARR Accumulation (Oct 2021 - May 2022)',fontsize=14)
-plt.savefig('NARRsnow_may2022_bc.png',bbox_inches = 'tight')
+#plt.savefig('NARRsnow_may2022_bc.png',bbox_inches = 'tight')
+
+plt.figure(figsize=(12,7))
+plt.contourf(np.flipud(snow2022_nonbc), cmap = 'BuPu', levels = np.linspace(0,3,11))
+#plt.contourf((distributed_accumulation_flipped), cmap = 'BuPu', levels = np.linspace(0,10,41))
+#plt.contourf(np.flipud(Topo), cmap = 'PuRd', levels = np.linspace(0,4, 20))
+legend = plt.colorbar()
+legend.ax.set_ylabel('Accumulation (m w.e.)', rotation=270,fontsize=14,labelpad=20)
+plt.xlabel('Easting',fontsize=14)
+plt.ylabel('Northing',fontsize=14)
+plt.xticks(ticks=[0,50,100,150,200,250,300],labels=[int(Xgrid[0][0]),int(Xgrid[0][50]),int(Xgrid[0][100]),int(Xgrid[0][150]),int(Xgrid[0][200]),int(Xgrid[0][250]),int(Xgrid[0][300])],fontsize=10,rotation=20)
+plt.yticks(ticks=[0,50,100,150,200],labels=[int(Ygrid[:,0][0]),int(Ygrid[:,0][50]),int(Ygrid[:,0][100]),int(Ygrid[:,0][150]),int(Ygrid[:,0][200])],fontsize=10,rotation=20)
+plt.title('Uncorrected NARR Accumulation (Oct 2021 - May 2022)',fontsize=14)
+#plt.savefig('NARRsnow_may2022_nonbc.png',bbox_inches = 'tight')
 
 def calculate_XYZAcc(Xgrid,Ygrid,Zgrid,Acc_array):
     '''
@@ -135,10 +150,10 @@ def calculate_XYZAcc(Xgrid,Ygrid,Zgrid,Acc_array):
     
     return easting_EY,northing_EY,elevation_EY,acc_EY
 
-easting_model, northing_model, z_model, narr_snow = calculate_XYZAcc(Xgrid,np.flipud(Ygrid),Zgrid,snow2022_bc)
+easting_model, northing_model, z_model, narr_snow_bc = calculate_XYZAcc(Xgrid,np.flipud(Ygrid),Zgrid,snow2022_bc)
 
 plt.figure(figsize=(12,7))
-plt.scatter(easting_model,northing_model,c=narr_snow,cmap="BuPu",s=3)
+plt.scatter(easting_model,northing_model,c=narr_snow_bc,cmap="BuPu",s=3)
 plt.clim(0,2.5)
 plt.scatter(easting_obs,northing_obs,c=obs_snow, cmap="BuPu",marker='o',linewidth=0.5,edgecolor='black',s=100) #levels = np.linspace(0,4.5,19))
 plt.clim(0,2.5)
@@ -147,7 +162,21 @@ legend.ax.set_ylabel('Accumulation (m w.e.)', rotation=270,fontsize=14,labelpad=
 plt.xlabel('Easting',fontsize=14)
 plt.ylabel('Northing',fontsize=14)
 plt.title('Bias Corrected NARR Accumulation (Oct 2021-May 2022) \n vs Snowpit Data (May 2022)',fontsize=14)
-plt.savefig('NARR_bcvsSnowpits2022.png',bbox_inches = 'tight')
+#plt.savefig('NARR_bcvsSnowpits2022.png',bbox_inches = 'tight')
+
+easting_model, northing_model, z_model, narr_snow_nonbc = calculate_XYZAcc(Xgrid,np.flipud(Ygrid),Zgrid,snow2022_nonbc)
+
+plt.figure(figsize=(12,7))
+plt.scatter(easting_model,northing_model,c=narr_snow_nonbc,cmap="BuPu",s=3)
+plt.clim(0,2.5)
+plt.scatter(easting_obs,northing_obs,c=obs_snow, cmap="BuPu",marker='o',linewidth=0.5,edgecolor='black',s=100) #levels = np.linspace(0,4.5,19))
+plt.clim(0,2.5)
+legend = plt.colorbar() #colorbar goes to whichever is second
+legend.ax.set_ylabel('Accumulation (m w.e.)', rotation=270,fontsize=14,labelpad=20)
+plt.xlabel('Easting',fontsize=14)
+plt.ylabel('Northing',fontsize=14)
+plt.title('Uncorrected NARR Accumulation (Oct 2021-May 2022) \n vs Snowpit Data (May 2022)',fontsize=14)
+#plt.savefig('NARR_nonbcvsSnowpits2022.png',bbox_inches = 'tight')
 
 #scatter plot w NARR on Y-Axis
 #find nearest neighbour gridcells...
@@ -170,7 +199,93 @@ def nearestneighbour(NARRsnowarray,NASAsnowlist=depth,NASAeasting,NASAnorthing=n
         
     return NARRaccumulation
 
-def nearestneighbour(NARRsnow,snowpitdata=obs_snow,snowpit_e=easting_obs,snowpit_n=northing_obs,snowpit_z=z_obs,Xgrid=Xgrid,Ygrid=np.flipud(Ygrid),Zgrid=Zgrid):
+def nearestneighbour(NARRsnow,snowpitdata=obs_snow,snowpit_e=easting_obs,snowpit_n=northing_obs,snowpit_z=z_obs,xgrid=Xgrid,ygrid=np.flipud(Ygrid),Zgrid=Zgrid):
+    distance_to_NN = []
+    NARRaccumulation = []
+    NARRelevs = []
     for i in range(0,len(snowpitdata)):
-        print(i)
+        x_dist = xgrid - snowpit_e[i]
+        y_dist = ygrid - snowpit_n[i]
+        distance = np.sqrt((x_dist**2)+(y_dist**2))
+        closest_cell = np.where(distance == np.nanmin(distance))
+        while np.isnan(NARRsnow[closest_cell]):
+            print('snow is nan in closest cell. searching for next closest cell')
+            distance[closest_cell] = 999
+            closest_cell = np.where(distance == np.nanmin(distance))
+        distance_to_NN.append(np.nanmin(distance))
+        NARRaccumulation.append(NARRsnow[closest_cell])
+        NARRelevs.append(Zgrid[closest_cell])
+        
+    return distance_to_NN, NARRaccumulation, NARRelevs
 
+NN_narr_snow_bc = nearestneighbour(snow2022_bc)
+NN_narr_snow_nonbc = nearestneighbour(snow2022_nonbc)
+
+plt.figure(figsize=(12,6))
+plt.suptitle('Nearest Neighbour Comparison',y=1)
+plt.subplot(1,2,1)
+plt.scatter(obs_snow,NN_narr_snow_nonbc[1],s=60)
+plt.title('Uncorrected NARR Accumulation (Oct 2021-May 2022) \n vs Snowpit Data (May 2022)')
+plt.xlim(0,2)
+plt.ylim(0,2)
+plt.plot([0,2],[0,2],linestyle='--',color='k')
+plt.xlabel('Snowpits (m w.e.)',fontsize=14)
+plt.ylabel('NARR Accumulation (m.w.e.)',fontsize=14)
+plt.subplot(1,2,2)
+plt.scatter(obs_snow,NN_narr_snow_bc[1],s=60)
+plt.title('Bias Corrected NARR Accumulation (Oct 2021-May 2022) \n vs Snowpit Data (May 2022)')
+plt.xlim(0,2)
+plt.ylim(0,2)
+plt.plot([0,2],[0,2],linestyle='--',color='k')
+plt.xlabel('Snowpits (m w.e.)',fontsize=14)
+plt.ylabel('NARR Accumulation (m.w.e.)',fontsize=14)
+#plt.savefig('NearestNeighbour_snowpits.png',bbox_inches = 'tight')
+
+plt.figure(figsize=(12,6))
+plt.suptitle('Nearest Neighbour Comparison',y=1)
+plt.subplot(1,2,1)
+for i in range(0,len(z_obs)):
+    plt.scatter(obs_snow[i],NN_narr_snow_nonbc[1][i],s=70)
+plt.title('Uncorrected NARR Accumulation (Oct 2021-May 2022) \n vs Snowpit Data (May 2022)')
+plt.xlim(0,2)
+plt.ylim(0,2)
+plt.legend(pitnames)
+plt.plot([0,2],[0,2],linestyle='--',color='k')
+plt.xlabel('Snowpits (m w.e.)',fontsize=14)
+plt.ylabel('NARR Accumulation (m.w.e.)',fontsize=14)
+plt.subplot(1,2,2)
+for i in range(0,len(z_obs)):
+    plt.scatter(obs_snow[i],NN_narr_snow_bc[1][i],s=70)
+plt.title('Bias Corrected NARR Accumulation (Oct 2021-May 2022) \n vs Snowpit Data (May 2022)')
+plt.xlim(0,2)
+plt.ylim(0,2)
+plt.plot([0,2],[0,2],linestyle='--',color='k')
+plt.xlabel('Snowpits (m w.e.)',fontsize=14)
+plt.ylabel('NARR Accumulation (m.w.e.)',fontsize=14)
+#plt.savefig('NearestNeighbour_snowpits.png',bbox_inches = 'tight')
+
+plt.figure(figsize=(6,6))
+plt.title('Nearest Neighbour Gridcell Comparison',y=1)
+#plt.subplot(1,2,1)
+plt.scatter(z_obs,NN_narr_snow_nonbc[2],s=70)
+#plt.title('Uncorrected NARR Accumulation (Oct 2021-May 2022) \n vs Snowpit Data (May 2022)')
+plt.xlim(1700,2600)
+plt.ylim(1700,2600)
+plt.plot([1700,2600],[1700,2600],linestyle='--',color='k')
+plt.xlabel('Snowpit elevations (m a.s.l.)',fontsize=14)
+plt.ylabel('NARR gridcell elevations (m a.s.l.)',fontsize=14)
+#plt.savefig('snowpitelevations.png',bbox_inches = 'tight')
+
+plt.figure(figsize=(6,6))
+plt.title('Nearest Neighbour Gridcell Comparison',y=1)
+#plt.subplot(1,2,1)
+for i in range(0,len(z_obs)):
+    plt.scatter(z_obs[i],NN_narr_snow_nonbc[2][i],s=70)
+#plt.title('Uncorrected NARR Accumulation (Oct 2021-May 2022) \n vs Snowpit Data (May 2022)')
+plt.xlim(1700,2800)
+plt.ylim(1700,2800)
+plt.legend(pitnames)
+plt.plot([1700,2800],[1700,2800],linestyle='--',color='k')
+plt.xlabel('Snowpit elevations (m a.s.l.)',fontsize=14)
+plt.ylabel('NARR gridcell elevations (m a.s.l.)',fontsize=14)
+#plt.savefig('snowpitelevations_coloured.png',bbox_inches = 'tight')
