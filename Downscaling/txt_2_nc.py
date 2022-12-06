54ab8a5e0c76b4d7a0a11c9a9a22b6fb450adf28
krobinson99 <kmr18@sfu.ca>
2022-12-06 14:37:01 -0800

    added input/output paths and fixed glacier_in file

--- Downscaling/txt_2_nc.py
+++ Downscaling/txt_2_nc.py
@@ -1,42 +1,36 @@
 ####Tool for converting large csv textfiles to netcdf####
 
-import matplotlib.dates as date
+#import matplotlib.dates as date
 import netCDF4
 from netCDF4 import Dataset
 import numpy as np
-import datetime as dt
+#import datetime as dt
+import os
 from Model_functions_ver4 import regridXY_something
 
-
-#name = 'GL_mid'  
-#name_list = ['Meltmid_dd', 'Meltmid_eddarc', 'Meltmid_eddre', 'MBGmid_dd', 'MBGmid_eddarc', 'MBGmid_eddre', 'Snowmid_dd', 'Snowmid_edd', 'Sradmid'] 
-
 name = 'kaskonly'
-#name_list =  [ 'Meltkaskice2017_dd']
 
-#name = 'WS' 
-#longform for all files 
-#name_list = ['Meltkaskice_dd', 'Meltkaskice_eddarc', 'MBGkaskice_dd', 'MBGkaskice_eddarc', 'Tempkaskice', 'Sradkaskice', 'Snowkaskice_dd', 'Snowkaskice_edd', 'precipaskice', 'netSnowkaskice']
-#name_list = ['Meltkasktopo_dd', 'Meltkasktopo_eddarc', 'MBGkasktopo_dd', 'MBGkasktopo_eddarc', 'Tempkasktopo', 'Sradkasktopo', 'Snowkasktopo_dd', 'Snowkasktopo_edd', 'precipkasktopo', 'netSnowkasktopo']
-#name_list = ['netSnowkask2010','netSnowkask2011','netSnowkask2012','netSnowkask2013','netSnowkaskonly2014','netSnowkaskonly2015', 'netSnowkaskonly2016','netSnowkaskonly2017','netSnowkaskonly2018']
-#name_list = ['Sradkask2006']
-#shortform for ETIM outpputs
-name_list = ['Tempkaskonly2006']
+path = 'D:/Downscaled_files/includes_CAtrib/2006-2022' 
+variable = 'netSnow'
+years = [2006]
+file_list = []
+for y in years:
+    fname = str(variable) + str(name) + str(y)
+    #file_list.append(os.path.join(path,fname))
+    file_list.append(fname)
 
-#WS outputs
-#name_list = ['TempWS', 'SradWS', 'precipWS']
+ref_path = 'F:/Mass Balance Model/Kaskawulsh-Mass-Balance/Ref_files'
 
-start_year = 2006
+start_year = years[0]
 
-for n in name_list:
+for n in file_list:
     print(n)
 
-    File_glacier_in = name + '.txt'
-    File_in = n + '.txt'
-    File_out = n + '.nc'
-    File_grid_out = name + '_grid.txt'
-    file_in = 'ref' + str(start_year) + '.nc'
-    print('using ' + file_in)
+    File_glacier_in = 'kaskonly_deb.txt'
+    File_in = os.path.join(path, str(n) + '.txt')
+    File_out = os.path.join(path, str(n) + '.nc')
+    File_grid_out = os.path.join(path, str(name) + '_grid.txt')
+    file_in = os.path.join(ref_path, 'ref' + str(start_year) + '.nc')
     
     fh = Dataset(file_in, "r")
     
@@ -44,19 +38,12 @@ for n in name_list:
     ###Extract grid geometry###
     
     glacier = np.genfromtxt(File_glacier_in, skip_header=1, delimiter=',')
-    
-    IH = glacier[:,6]
-    inval_loc = np.where(IH == -9999)
-    IH[inval_loc] = IH[inval_loc] * np.nan
-    Ih = IH
-#Ih = np.delete(IH, inval_loc)
-    
-    IX = glacier[:,4]
-    Ix = IX
-#    Ix = np.delete(IX, inval_loc)
-    IY = glacier[:,5]
-    Iy = IY
-#    Iy = np.delete(IY, inval_loc)
+    Ih = glacier[:,2]
+    inval_loc = np.where(Ih == -9999)
+    Ih[inval_loc] = Ih[inval_loc] * np.nan
+    Ix = glacier[:,3]
+    Iy = glacier[:,4]
+
     
     #note that Zgrid is also used as a nan mask
     Zgrid, Xgrid, Ygrid, xbounds, ybounds = regridXY_something(Ix, Iy, Ih)
