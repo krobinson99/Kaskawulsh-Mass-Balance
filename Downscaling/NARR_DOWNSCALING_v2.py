dbd590f4a319b234d867bf749bb943c08131e7fe
krobinson99 <kmr18@sfu.ca>
2023-08-01 15:15:31 -0700

    reverted to May1 commit #1, aesthetic/clean-up only

--- Downscaling/NARR_DOWNSCALING_v2.py
+++ Downscaling/NARR_DOWNSCALING_v2.py
@@ -32,41 +32,54 @@ sys.path.insert(1,'F:\Mass Balance Model\Kaskawulsh-Mass-Balance\RunModel')
 from Model_functions_ver4 import write_config_file
 
 #Import parameters from config file
-from DOWNSCALINGnamelist import start_year
-from DOWNSCALINGnamelist import end_year
-from DOWNSCALINGnamelist import glacier_id
+from DOWNSCALINGnamelist import start_year, end_year
+from DOWNSCALINGnamelist import Glacier_ID
 from DOWNSCALINGnamelist import glacier_outline
 from DOWNSCALINGnamelist import static_surface
 from DOWNSCALINGnamelist import UTM
 from DOWNSCALINGnamelist import NARR_subregions
 from DOWNSCALINGnamelist import normalized_XYZ
 from DOWNSCALINGnamelist import delta_t
-from DOWNSCALINGnamelist import downscaled_outputs
+from DOWNSCALINGnamelist import OUTPUT_PATH
 from DOWNSCALINGnamelist import catchment
-from DOWNSCALINGnamelist import rawnarr_inputs
-from DOWNSCALINGnamelist import solar_in
+from DOWNSCALINGnamelist import Climate_inputs, Coarse_DEM_input, Easting_grid, Northing_grid
 
-#set path for downscaled outputs(Temp, Precip, & Radiation) to go into 
-OUTPUT_PATH = downscaled_outputs
-# save the namelist that was used here into a txt file that will go in the outputs directory
+# Save configuration file for this run to output directory:
 write_config_file(OUTPUT_PATH,"DOWNSCALINGnamelist.py")
 
-# KR_note: a bunch of things only need to happen ONCE - not once per year. Move those things out of the for loop:
+# Load coarse NARR grid:
+# =============================================================================
+NARR_DEM = Dataset(Coarse_DEM_input, "r")
+coarse_elev =  NARR_DEM.variables['hgt'][0,:,:] # surface geopotential height (time invariant), units = m a.s.l.
 
-# Load coarse NARR grid
-File_CoarseElev_in = 'kaskCE.nc'
-inCE = Dataset(File_CoarseElev_in, "r")
-CE_array = inCE.variables['hgt']
-elev = CE_array[0,:,:] # surface geopotential height, units = m, shape = (6,6)
-
-print("Coarse NARR DEM loaded...")
+lons = NARR_DEM.variables['lon'][:]
+lats = NARR_DEM.variables['lat'][:]
 sys.stdout.flush()
+
+Projection = Proj('+proj=utm +zone=' + UTM + ', +north +ellps=WGS84 +datum=WGS84 +units=m +no_defs')
+UTMy, UTMx = Projection(lons, lats)  # converts lat/lons of coarse NARR grid to easting, northing on WGS84 projection.
+
+#create list of array positions ungridded
+UTMx_list = UTMx.ravel()
+UTMy_list = UTMy.ravel()
+grid_pts = np.stack((UTMx_list, UTMy_list)) #KR_note: not needed, can be replaced with UTM_xlist/ylist further down in code
+print("Coarse NARR grid loaded")
+# =============================================================================
+
+
+# Load grid to downscale onto:
+# =============================================================================
+Xgrid = np.loadtxt(Easting_grid)
+Ygrid = np.loadtxt(Northing_grid)
+print('Model grid loaded')
+# =============================================================================
 
 # Load model domain:
 File_glacier_in = glacier_outline + '.txt' 
 glacier = np.genfromtxt(File_glacier_in, skip_header=1, delimiter=',')
 
 #Extract grid geometry, correcting for dem/shapefile non-overlaps
+# KR_note: delete all this stuff once downscaling is set up for arrays
 if static_surface == True:
     if catchment == False:
         IH = glacier[:,2]
@@ -83,24 +96,20 @@ if static_surface == True:
     print('Static Surface Loaded')
 else:
     pass
-    
-#print('Domain loaded')
 
- # KR_note: the key here will be figuring out how regridXYsomething takes Ih, Ix, Iy and makes Zgrid
- # then do the reverse on the new Zgrids to make them compatible with this code. 
- # Ih, Iy, Iz are the NON-NAN gridcells only. 
- # idea: make np.zeros(Zgrid.shape), replace nanlocs with NaNs, then loop through gridcells and
- # wherever cell == 0, fill with first element of Ih, then repeat for each gridcell w a zero. (try row wise and column wise)
- # if that method works and creates the right Zgrid, then reverse that to get Ih lists from Zgrid_dynamic. 
-
-#set up time steps in a day...
-time_steps = np.arange(0,24,delta_t) #timeinterv
-
-# Define years to downscale
+# Setting-up time scale and timesteps
+# =============================================================================
 years = np.arange(start_year,end_year+1)
+time_steps = np.arange(0,24,delta_t)
+print('Downscaling years:',list(years))
+# =============================================================================
 
+
+# Begin looping through years: 
+# =============================================================================
 for year in years:
     print(year)
+    # KR_note: delete once downscaling is set yp for arrays, but keep option to have static vs dynamic surface
     if static_surface == False:
         Ih_file = 'Ih_' + str(year) + '.txt'
         Ih = np.loadtxt(Ih_file)
@@ -109,57 +118,40 @@ for year in years:
         print('Dynamic Surface Loaded')
     else:
         pass
-    #load inputs:
     
-    File_elev_in = os.path.join(rawnarr_inputs,'kaskhgt.' + str(year) + '.nc')
-    File_temp_in = os.path.join(rawnarr_inputs,'kaskair.' + str(year) + '.nc')
-    File_precip_in = os.path.join(rawnarr_inputs,'kaskapcp.' + str(year) + '.nc')
+    # Load DEM: 
+    Zgrid = np.loadtxt('Ih_' + str(year) + '.txt') #KR_note: replace this with 2d DEM array for each year: use consistent naming convention
     
+    File_elev_in = os.path.join(Climate_inputs,str(Glacier_ID) + '_hgt.' + str(year) + '.nc')
+    File_temp_in = os.path.join(Climate_inputs,str(Glacier_ID) + '_air.' + str(year) + '.nc')
+    File_precip_in = os.path.join(Climate_inputs,str(Glacier_ID) + '_apcp.' + str(year) + '.nc')
+    
+    # Load NARR inputs:
     inE = Dataset(File_elev_in, "r")
-    E_var = 'hgt'
     sys.stdout.flush()
-    E_array = inE.variables[E_var][:]
+    E_array = inE.variables['hgt'][:]
     
     inT = Dataset(File_temp_in, "r")
-    T_var = 'air'
     sys.stdout.flush()
-    T_array = inT.variables[T_var][:]
+    T_array = inT.variables['air'][:]
     
     inP = Dataset(File_precip_in, "r")
-    P_var = 'apcp'
     sys.stdout.flush()
-    P_array = inP.variables[P_var][:]
-    
-    print("Raw NARR inputs loaded....") 
-    
-    #Load solar data:
-    if catchment == False:
-        solar_prefix = 'fixed' + 'kaskonly' + '_' #GLright/left/mid, Kaskice, kasktopo #glacier solar files
-        solar_suffix = 'DS.txt'
-        #EDIT! lines above should be uncommented: doing a test to see if the solar files can match Ih from kaskonly_deb
-        #solar_prefix = 'fixed' + 'kask' + '_' #GLright/left/mid, Kaskice, kasktopo #catchment solar files 
-        #solar_suffix = 'DS.txt'
-    else:
-        solar_prefix = 'fixed' + 'kask' + '_' #GLright/left/mid, Kaskice, kasktopo #catchment solar files 
-        solar_suffix = 'DS.txt'
-                    
+    P_array = inP.variables['apcp'][:]
+    print("NARR Geopotential, Temperatue, Precipitation inputs loaded....") 
+                   
                         #######OUTPUTS#######
     
     #Prep output file and variables
     print('Setting up files for downscaled outputs')
     
     #TEMPERATURE:
-    Temp_outfile_name = 'Temp' + glacier_id + str(year) + '.txt'
+    Temp_outfile_name = 'Temp_' + str(year) + '.txt'
     Temp_output_path = os.path.join(OUTPUT_PATH,Temp_outfile_name)
     Temp_out = open(Temp_output_path, 'w')
     
-    #SOLAR RADIATION:
-    Srad_outfile_name = 'Srad' + glacier_id + str(year) + '.txt'
-    Srad_output_path = os.path.join(OUTPUT_PATH,Srad_outfile_name)
-    SR_out = open(Srad_output_path, 'w')
-    
-    #ACCUMULATION:
-    netSnow_outfile_name = 'netSnow' + glacier_id + str(year) + '.txt'
+    #PRECIPITATION:
+    netSnow_outfile_name = 'Precip' + Glacier_ID + str(year) + '.txt'
     netSnow_output_path = os.path.join(OUTPUT_PATH,netSnow_outfile_name)
     Precip_out = open(netSnow_output_path, 'w')
     
@@ -174,20 +166,7 @@ for year in years:
         date_list.append(d)
     
     dt_3hourly_dayonly = np.array(date_list)
-
-    #Get UTM values for each reanalysis grid point to cycle between nodes on larger 
-    #modelled glaciers
-    lons = inE.variables['lon'][:]
-    lats = inE.variables['lat'][:]
     
-    myProj = Proj('+proj=utm +zone=' + UTM + ', +north +ellps=WGS84 +datum=WGS84 +units=m +no_defs')
-    UTMx, UTMy = myProj(lons, lats)  # converts lat/lons of coarse NARR grid to easting, northing on WGS84 projection.
-    
-    #create list of array positions ungridded
-    UTMx_list = UTMx.ravel()
-    UTMy_list = UTMy.ravel()
-    grid_pts = np.stack((UTMy_list, UTMx_list)) #KR_note: not needed, can be replaced with UTM_xlist/ylist further down in code
-        
                       #######MODEL#######
 
     ###Begin MB calculations, where dates are used to toggle while loop which
@@ -207,9 +186,6 @@ for year in years:
         dailyT = T_array[hourly_indices]
         dailyP = P_array[daily_index]
     
-        Daily_solar = np.genfromtxt(os.path.join(solar_in,solar_prefix + str(DOY) + solar_suffix), delimiter=' ')
-        solar_interval = 0.5 # Daily_solar has 30 minute vals of solar radiation (48 per day) for every gridcell in the domain (same shape as Ih/Ix/Iy)
-         
         for i in range(0, len(time_steps)): #Looping through each timestep (8 per day)
             ###Get values for current hour, indexing is yucky, but straighforward
             ###Note that units for NARR precip are mm we, conversion is done here  
@@ -237,12 +213,12 @@ for year in years:
             # See Young et al. (2021) supplementary material for more info.
             
             if i == 0: #Only need to do once per day
-                r_beta2, b_coeffs, b0 = rainy_day_funk(elev.ravel()[NARR_subregions], hourlyP.ravel()[NARR_subregions], UTMy_list[NARR_subregions], UTMx_list[NARR_subregions]) 
+                r_beta2, b_coeffs, b0 = rainy_day_funk(coarse_elev.ravel()[NARR_subregions], hourlyP.ravel()[NARR_subregions], UTMx_list[NARR_subregions], UTMy_list[NARR_subregions]) 
                 if normalized_XYZ == True:
                     Xmax = np.max(UTMx_list[NARR_subregions])
                     Ymax = np.max(UTMy_list[NARR_subregions])
-                    Zmax = np.max(elev.ravel()[NARR_subregions])
-                    r_beta2, b_coeffs, b0 = rainy_day_funk(elev.ravel()[NARR_subregions]/Zmax, dailyP.ravel()[NARR_subregions], UTMy_list[NARR_subregions]/Ymax, UTMx_list[NARR_subregions]/Xmax)     
+                    Zmax = np.max(coarse_elev.ravel()[NARR_subregions])
+                    r_beta2, b_coeffs, b0 = rainy_day_funk(coarse_elev.ravel()[NARR_subregions]/Zmax, dailyP.ravel()[NARR_subregions], UTMx_list[NARR_subregions]/Xmax, UTMy_list[NARR_subregions]/Ymax)     
                 else:
                     pass
             else:
@@ -260,8 +236,8 @@ for year in years:
                 NARR_cell = closest_node(downscaled_cell, grid_pts) #Finds which NARR gridcell (36 total) is closest to the gridcell being downscaled.
             
                 #use index to get nearest grid point in u, w notation
-                u = int(np.where(UTMy == grid_pts[0][NARR_cell])[0])
-                w = int(np.where(UTMx == grid_pts[1][NARR_cell])[1])
+                u = int(np.where(UTMx == grid_pts[0][NARR_cell])[0])
+                w = int(np.where(UTMy == grid_pts[1][NARR_cell])[1])
                 
                 #KR_note: edit/move these comments when done.
                 #find downscaled value at every grid point. If outside bounds, 
@@ -302,35 +278,18 @@ for year in years:
                 else:
                     Pdownscaled = 0.
 
-                #Calculate SR/GR enhancement for both reanalysis dataset
-                #add sum function when using hourly arc SR. NOTE: code exists for
-                #averaged SR scheme in previous verison of this model
-                #UNCOMMENT
-                SR_local = Daily_solar[z , :] # 30 minute solar values for current day and current gridcell
-                SR = SR_local[int(time_steps[i]/solar_interval)]
-                
                 #Update lists  
                 Thour.append(K)
                 Phour.append(Pdownscaled)
-                SRhour_arc.append(SR)
 
             
-            # Srad updated to units per hour
-            SRhour_ARC = np.asarray(SRhour_arc)/solar_interval
-           
 
             # Write downscaled variable to the output files:
             MBwrite = Thour
             for im in MBwrite:
                 Temp_out.write("%s," %im) #prepare for next row/day by next line 
             Temp_out.write("\n")
-            
-            MBwrite = SRhour_ARC
-            for im in MBwrite:
-                SR_out.write("%s," %im) #prepare for next row/day by next line 
-            SR_out.write("\n")
-            
-            
+                                    
             MBwrite = Phour
             for im in MBwrite:
                 Precip_out.write("%s," %im) #prepare for next row/day by next line 
@@ -338,16 +297,11 @@ for year in years:
     
 
     Temp_out.close()
-    SR_out.close()
     Precip_out.close()
     inT.close()
     inE.close()
     inP.close()
        
-    print(glacier_id + ' Complete')
+    print(Glacier_ID + 'Downscaling Complete')
     
     # KR_note: edited up to here Mar 7 2023: everything below is not edited yet
-
-
-    
-    
