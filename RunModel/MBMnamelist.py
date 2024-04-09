5a4489172ea0d8159281ddb01158bfa8b4908782
krobinson99 <kmr18@sfu.ca>
2024-04-08 17:31:12 -0700

    updated namelist to set parameters for mass-balance model

--- RunModel/MBMnamelist.py
+++ RunModel/MBMnamelist.py
@@ -5,94 +5,38 @@ Created on Tue May 25 10:15:08 2021
 @author: katierobinson
 """
 
-#time_control
-time_step = 10800     #timestep - keep in units of seconds so that this is more flexible?
-start_year = 2007
-end_year = 2020
-start_day = 1
-#domain
-glacier_id = 'kaskonly'       #points to a specific glacier outline --> dont change this: it s the same for the kaskawulsh inputs and catchment inputs
-Considering_Catchment = True #are we simulating the whole catchment? (True) or just the Kask glacier (False)
+# Time control
+start_year = 2015
+end_year = 2018
+timestep = 3      # Should match timestep of input data. Units = hours.
 
-#parameterizations
-#debris
-save_MB_only = True #save net mb as npy file instead of netcdf. Makes model run much faster!
-###############################################################################################################
-debris = True  #turn debris on or off
-debris_treatment = 'Variable Thickness' # 'OriginalBoolean', 'Boolean' or 'Variable Thickness'
-deb_uncertainty_test_peakMthickness = False #leave off (False) if using ref value only
-deb_uncertainty_test_transitionthickness = False #leave off (False) if using ref value only
-###############################################################################################################
+# Input parameters
+params_file = 'F:/Mass Balance Model/Kaskawulsh-Mass-Balance/Tuning/Tuning_Params_Initial10k.csv' # Path to csv file containing MF, aice, asnow.
+sim = 666                                             # Corresponds to a row of the params file.
+R2S = 1.0                                                  # Rain-to-snow threshold (degree C).
+Glacier_ID = 'KRH'                                         # Identifier for the glacier. All inputs/outputs will also have the same ID.
 
-#don't edit these settings unless changing a reference value or path, otherwise keep the same for all simulations
-###############################################################################################################
-b0 = 11.0349260206858 #b0 and k are params fitted to KW from Rounce et al. (2021)
-k = 1.98717418666925 
-cleaniceM = 2.0277000000000003 #observed dirty ice melt #2.9200183038347 (DR) #m w.e.
-peakM = 2.1717000000000004 #observed peak melt #6.62306028549649 (DR) #m w.e.
-peakM_thickness_ref = 0.006 # m (reference value)
-transition_thickness_ref = 0.019 #m (reference value)
+# Input data
+Model_functions = 'F:\Mass Balance Model\Kaskawulsh-Mass-Balance\RunModel' # Path to folder where model functions script is.
+Precip_inputs = 'D:/BiasCorrected_files/KRH'                                 # Path to folder where downscaled & bias-corrected data is.
+Temp_inputs = 'D:/BiasCorrected_files/KRH'
+Solar_inputs = 'D:/Solar_files/KRH'
 
-if deb_uncertainty_test_peakMthickness == True:
-    peakthickness_uncertainty = 0.003
-else:
-    peakthickness_uncertainty = 0
-    
-if deb_uncertainty_test_transitionthickness == True:
-    transitionthickness_uncertainty = 0.007
-else:
-    transitionthickness_uncertainty = 0
+# Input geometry
+Easting_grid = 'F:/Mass Balance Model/Kaskawulsh-Mass-Balance/Downscaling/KRH_Xgrid.txt' # Paths to text files defining Easting/Northing coords of every model gridcell
+Northing_grid = 'F:/Mass Balance Model/Kaskawulsh-Mass-Balance/Downscaling/KRH_Ygrid.txt'
+Sfc_grid = 'F:/Mass Balance Model/Kaskawulsh-Mass-Balance/RunModel/KRH_SfcType.txt'      # Path to text file where 1 = off-glacier, 0 = on-glacier, NaN = not in the domain.
 
-if debris_treatment == 'Boolean':
-    debris_thickness_map = 'F:\Mass Balance Model\Kaskawulsh-Mass-Balance\RunModel\debmap_boolean.npy' #path to map 
-elif debris_treatment == 'Variable Thickness': 
-    debris_thickness_map = 'F:\Mass Balance Model\Kaskawulsh-Mass-Balance\RunModel\debmap_variableh.npy' #path to map 
-###############################################################################################################
+# Debris parameters
+debris_parameterization = 'Sub-debris melt scaling' # Debris treatment. Options are (1) None (no debris), (2) 'Boolean debris' (set a_ice = 0 in debris cells), or (3) 'Sub-debris melt scaling' (thickness dependent scaling)
+debris_map = 'F:/Mass Balance Model/Kaskawulsh-Mass-Balance/DebrisThickness/KRH_debrismap.txt' # Path to text file. If debris parameterization is (1) file should be same as Sfc; (2) debris cells are finite, else NaN, (3) cells w debris have value = debris thickness, else NaN
+cleanice_melt = 2.0277           # Observed clean ice melt (m w.e).
+peak_melt = 2.1717               # Observed peak melt (m w.e.).
+peak_melt_thickness = 0.006      # Thickness at which peak melt occurs (m).
+transition_thickness = 0.019     # Thickness at which melt equals clean ice melt (m).
+b0 = 11.0349260206858            # b0 and k are site-specific params from Rounce et al. (2021).
+k = 1.98717418666925
 
-Rain_to_snow = 1   #set the rain to snow threshold (line ~132)
-Refreezing = True
-Temp_shift = False # do you want to change the entire temperature array up or down by a uniform constant? True = yes 
-temp_shift_factor = 0 # what is the temperature shift. + is an increase in temp, - is a decrease
-Bias_CorrectionT = True #are you using bias corrected temp files as input (True) or not (False)
-Bias_CorrectionP = True #are you using bias corrected precip files as input (True) or not (False)
-
-Tuning = True
-param_total = 2 #how many parameter combinations to generate for the tuning process
-JointProbabilityDistribution = False #true: select parameters from joint probability distribution, if false, select from full distribution 
-#mean parameter values and covariance matrix, needed for generating the multivariate gaussian distribution: defined in Tuning/JointProbabilityDistribution.py
-means_debriscase = [2.97570389e-04, 2.44364946e-06, 1.02574550e-06]
-covariance_debriscase = [ 8.59048888e-09, -3.10636027e-11, -3.39930610e-11],[-3.10636027e-11,  1.88881027e-13,  1.20383794e-13],[-3.39930610e-11,  1.20383794e-13,  1.45712997e-13]
-
-means_litvals = [2.66355e-04, 2.54450e-06, 1.59065e-06]
-covariance_litvals = [ 2.57039900e-08, -8.87368158e-12, -3.49816639e-11],[-8.87368158e-12,  9.89741842e-13,  7.55436921e-13],[-3.49816639e-11,  7.55436921e-13,  7.72041924e-13]
-
-#file_names
-#Input_path = .../.../… #tell the model where to find the input files
-params_filename = 'final_params_deb.csv' #tell the model which file contains the parameters we want to use for this run
-#where are the downscaled/bias corrected inputs stored
-#T_inputs = 'D:\\Katie\\Mass Balance Model\\MassBalanceModel_KatiesVersion\\Final_runs'
-
-#File_glacier_in = ?? #need to change so that debris/non-debris case is more clear
-Output_path = 'D:/TuningOutputs/Test_0723_throwaway' #tell the model where to put the output netcdf files
-ref_file_path = 'F:/Mass Balance Model/Kaskawulsh-Mass-Balance/Ref_files'
-File_sufix = ".nc"
-
-#######################################################################################################
-if Bias_CorrectionT == True:
-    #T_inputs = 'F:\\Mass Balance Model\\BiasCorrectedInputs_old\\Kaskonly_R2S=1'
-    T_inputs = 'D:/BiasCorrected_files/Catchment/Final_Inputs_1979-2022'
-else:
-    T_inputs = 'F:\\Mass Balance Model\\Kaskonly_Downscaled_NoBC'
-
-if Bias_CorrectionP == True:
-    #P_inputs = 'F:\\Mass Balance Model\\BiasCorrectedInputs_old\\Kaskonly_R2S=1'
-    P_inputs = 'D:/BiasCorrected_files/Catchment/Final_Inputs_1979-2022'
-else:
-    P_inputs = 'F:\\Mass Balance Model\\Kaskonly_Downscaled_NoBC'
-
-if Considering_Catchment == True:
-    SR_inputs = 'D:/Downscaled_files/Catchment/DynamicSurface'
-else:
-    #SR_inputs = 'F:/Mass Balance Model/Downscaled_files/missing_trib/Kaskonly_Downscaled_NoBC'
-    SR_inputs = 'D:/BiasCorrected_files/kaskonly_newcorrection_2013-2019'
-
+# Outputs
+OUTPUT_PATH = 'D:/TuningOutputs/Tuning_Nov8_TESTING_ALL_OUTPUTMODES'                   # Path to directory where model outputs should be saved
+SaveMBonly = True                        # If true, model saves mass balance only. If false, model saves Ice Melt, Snow Melt, Refreezing, Mass Balance
